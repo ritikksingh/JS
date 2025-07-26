15191220ea43b20d17003f35577138aad1709f44
RITIK  SINGH <106300231+ritikksingh@users.noreply.github.com>
2025-07-26 09:28:04 +0000

    variable complete

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "ritik9978@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+// accountId = 2   //not allow
+
+console.log(accountId);
+accountEmail = "sanu@57767"
+accountPassword = "9876"
+accountCity = "mirzapur"
+let accountState;
+
+
+/*
+prefer not to use var 
+because of issu in functional scope and block scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
