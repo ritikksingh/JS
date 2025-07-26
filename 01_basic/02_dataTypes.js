ced84e9c570640b66a2bfbaa57dd1f08bab314c7
RITIK  SINGH <106300231+ritikksingh@users.noreply.github.com>
2025-07-26 09:59:43 +0000

    dataType

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  // we are using node.js not browser
+
+let name = "ritik"
+let age = 21
+let isLoggedIn= false
+let state;
+
+
+//number => 2^53
+//bigint
+//string " "
+// boolean => true/false
+// null=  standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof undefined);  //type=> undefined
+console.log(typeof null);  // Object
