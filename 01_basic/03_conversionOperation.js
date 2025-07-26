231e88f0e37dcc601abb8f671117c416208f5298
RITIK  SINGH <106300231+ritikksingh@users.noreply.github.com>
2025-07-26 12:11:23 +0000

    conversion of DataType

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "44abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber); //NAN
+
+
+// "33"=> 33
+// "33abc"=> NaN
+
+
+let isLoggedIn= 1
+let booleanisLoogedIn = Boolean(isLoggedIn)
+console.log(booleanisLoogedIn);
+
+// true=> 1 false=> 0
+// "" => false
+//"Ritik" => true
+
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
