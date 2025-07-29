db2b9ff0d1b6167dbf3452ad02f8638b2f1b75af
RITIK  SINGH <106300231+ritikksingh@users.noreply.github.com>
2025-07-29 04:09:43 +0000

    Array part 1

---
+++ 02_basic/01_Array.js
@@ -0,0 +1,18 @@
+//Array
+
+const myArr = [0,1,2,3,4,5]
+const myHero = ["shaktiman","Netaji"]
+
+const myArr2 = new Array(1,3,5,7)
+// console.log(myArr[0]);
+
+
+//Arrray Method
+// myArr.push(6)
+// myArr.unshift(5)
+// myArr.shift(5)
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
