b316a519aa7ea33040dc391dcf080581b6c3e2ce
RITIK  SINGH <106300231+ritikksingh@users.noreply.github.com>
2025-08-03 17:23:09 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,57 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("fabruary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    
+
+    default: console.log("default match");
+    
+        break;
+}
+
+
+
+
+const mahina = "april"
+switch (mahina) {
+    case"jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("fabruary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "april":
+        console.log("april");
+        break;
+   
+
+    default: console.log("default match");
+    
+        break;
+}
