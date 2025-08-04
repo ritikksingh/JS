2c842e763cc5431d31edcedf3b1c0a7d80f99755
RITIK  SINGH <106300231+ritikksingh@users.noreply.github.com>
2025-08-04 19:16:14 +0000

    for in loop  in Object

---
+++ 05_iterations/four.js
@@ -0,0 +1,38 @@
+const myObject = {
+    js : "javascript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "swift by apple"
+}
+
+for (const key in myObject) {
+   
+    //    console.log(`${key} shortcut is for ${myObject[key]}`);
+       
+        
+}
+
+
+const programming = ["js", "ruby","python" ,"java"]
+for (const key in programming) {
+
+    // console.log(key);
+    
+    // console.log(programming[key]);
+    
+}
+
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United State of America")
+// map.set('Fr', "france")
+// for (const key in map) {
+//    console.log(key);
+   
+// }
+
+
+
+///Map is not iteratable
